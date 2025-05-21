5b59f3bb3dc05b5b41c42b78077cd1fcc85ad963
karthik Duggana <d.karthikyadav94@gmail.com>
2025-05-21 17:43:29 +0530

    terraform

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,6 +1,11 @@
-output "public_ips" {
-  value = concat(
-    [aws_instance.master_node.public_ip],
-    [for instance in aws_instance.worker_node : instance.public_ip]
-  )
+output "master_node_ip" {
+  value = aws_instance.master_node.public_ip
+}
+
+output "worker_node_1_ip" {
+  value = aws_instance.worker_node[0].public_ip
+}
+
+output "worker_node_2_ip" {
+  value = aws_instance.worker_node[1].public_ip
 }
