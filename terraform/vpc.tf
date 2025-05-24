c6796750058336ed2bc8168c168b3fd252c1f4ae
karthik Duggana <d.karthikyadav94@gmail.com>
2025-05-24 14:22:04 +0530

    jenkins

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -87,6 +87,13 @@ resource "aws_security_group" "worker_node" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    description = "jenkins"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   ingress {
     description = "Kubelet API"
